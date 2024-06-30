f316def1e6a6b8200715b187bee120fdd8844937
roitium <zdc666nb@gmail.com>
2024-06-30 21:35:11 +0800

    :sparkles: feat: add 404 page and update layout metadata
    - :page_facing_up: Added `app/not-found.tsx` with a custom 404 page message.
    - :art: Updated `app/layout.js` to include Open Graph protocol metadata for better SEO and social media sharing.
    - :wrench: Updated `package.json` to use wildcard versions for dependencies and devDependencies for easier future updates.

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,11 @@ export default function RootLayout({children}) {
         <html>
         <head>
             <link rel="icon" href="/favicon.ico" sizes="any"/>
+            {/*for OG protocol*/}
+            <meta property="description" content="Next generation frontend for haofenshu.com"/>
+            <meta property="og:title" content="HFS NEXT - 下一代好分数"/>
+            <meta property="og:description" content="Next generation frontend for haofenshu.com"/>
+            <meta property="twitter:card" content="summary_large_image"/>
             <title>HFS NEXT - 下一代好分数</title>
         </head>
         <body>
