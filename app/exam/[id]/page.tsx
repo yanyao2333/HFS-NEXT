43ecc8f324b0dd331dc1eff368f7d03a444d3d58
Roitium. <65794453+yanyao2333@users.noreply.github.com>
2024-08-12 00:14:37 +0800

    [feat] Add toast

--- app/exam/[id]/page.tsx
+++ app/exam/[id]/page.tsx
@@ -308,10 +308,13 @@ export default function ExamPage({params}: { params: { id: string } }) {
                             <div onClick={() => {
                                 toast.promise(createScreenshot(), {
                                     loading: "正在截图",
-                                    success: "成功创建并下载截图！",
+                                    success: 成功创建并下载截图！<br>（答题卡图片空白是正常的）,
                                     error: (err) => "创建截图失败，原因：" + err
                                 }, {
                                     error: {
+                                        duration: 5000
+                                    },
+                                    success: {
                                         duration: 5000
                                     }
                                 })
