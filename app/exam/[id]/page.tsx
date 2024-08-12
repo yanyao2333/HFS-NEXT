b1cf3e018cffa819c79fe78b733b33d1e880b5bb
roitium <zdc666nb@gmail.com>
2024-08-12 21:45:55 +0800

    [refactor]

--- app/exam/[id]/page.tsx
+++ app/exam/[id]/page.tsx
@@ -1,164 +1,17 @@
 "use client"
 import {Card, CardContent, CardHeader, CardTitle} from "@/components/card"
 import {useEffect, useRef, useState} from "react"
-import {
-    getAnswerPictureAction,
-    getExamOverviewAction,
-    getExamRankInfoAction,
-    getPaperRankInfoAction,
-    getUserSnapshotAction
-} from "@/app/actions";
+import {getExamOverviewAction, getExamRankInfoAction, getUserSnapshotAction} from "@/app/actions";
 import {useRouter, useSearchParams} from "next/navigation";
 import {formatTimestamp} from "@/utils/time";
-import {AppRouterInstance} from "next/dist/shared/lib/app-router-context.shared-runtime";
 import Navbar from "@/components/navBar";
-import {ExamDetail, ExamRankInfo, Paper, PaperRankInfo, UserSnapshot} from "@/types/exam";
+import {ExamDetail, ExamRankInfo, UserSnapshot} from "@/types/exam";
 import Snapshot from "@/app/exam/[id]/snapshot";
 import {Radar} from "react-chartjs-2";
-import {Chart as ChartJS, Filler, Legend, LineElement, PointElement, RadialLinearScale, Tooltip,} from 'chart.js';
+import {Chart as ChartJS, Filler, Legend, LineElement, PointElement, RadialLinearScale, Tooltip} from 'chart.js';
 import toast from "react-hot-toast";
 import html2canvas from "html2canvas";
-
-function PaperHiddingComponent(props: { paper: Paper, changeDisplayMode: Function }) {
-    return (
-        <Card>
-            <CardHeader onClick={() => {
-                props.changeDisplayMode(props.paper.paperId)
-            }} className="select-none cursor-pointer">
-                <div className="flex w-full items-center justify-between">
-                    <div>{props.paper.name}</div>
-                    <div className="flex items-center gap-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
-                             stroke="currentColor" className="size-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"/>
-                        </svg>
-                    </div>
-                </div>
-            </CardHeader>
-        </Card>
-    )
-}
-
-function PaperShowingComponent(props: {
-    paper: Paper,
-    changeDisplayMode: Function,
-    advancedMode: boolean,
-    router: AppRouterInstance,
-    examId: string
-}) {
-    let [paperRankInfo, setPaperRankInfo] = useState<PaperRankInfo>();
-    let [answerPictureUrls, setAnswerPictureUrls] = useState<string[]>([]);
-    useEffect(() => {
-        const token = localStorage.getItem("hfs_token")
-        if (!token) {
-            toast.error("你还没登录诶！")
-            props.router.push("/")
-            return
-        }
-        getPaperRankInfoAction(token, props.examId, props.paper.paperId).then((exams) => {
-            if (!exams.ok) {
-                toast.error("获取试卷失败：" + exams.errMsg)
-                return
-            }
-            if (!exams.payload) {
-                toast.error("获取试卷失败：" + exams.errMsg)
-                return
-            }
-            setPaperRankInfo(exams.payload)
-        })
-        getAnswerPictureAction(token, props.paper.paperId, props.paper.pid, props.examId).then((exams) => {
-            if (!exams.ok) {
-                toast.error("获取试卷失败：" + exams.errMsg)
-                return
-            }
-            if (!exams.payload) {
-                toast.error("获取试卷失败：" + exams.errMsg)
-                return
-            }
-            setAnswerPictureUrls(exams.payload["url"])
-        })
-    }, [props.examId, props.paper.paperId, props.paper.pid, props.router]);
-
-    return (
-        <Card>
-            <CardHeader onClick={() => {
-                props.changeDisplayMode(props.paper.paperId)
-            }} className="select-none cursor-pointer">
-                <div className="flex w-full items-center justify-between">
-                    <div>{props.paper.name}</div>
-                    <div className="flex items-center gap-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
-                             stroke="currentColor" className="size-6">
-                            <path strokeLinecap="round" strokeLinejoin="round"
-                                  d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3"/>
-                        </svg>
-                    </div>
-                </div>
-            </CardHeader>
-            <CardContent className="grid gap-4 px-4 pb-4 select-none">
-                <div className="grid grid-cols-2 gap-4">
-                    <div>
-                        <div className="text-sm text-gray-500 dark:text-gray-400">满分</div>
-                        <div className="font-medium">{props.paper.manfen}</div>
-                    </div>
-                    <div>
-                        <div className="text-sm text-gray-500 dark:text-gray-400">得分</div>
-                        <div className="font-medium">{props.paper.score}</div>
-                    </div>
-                </div>
-                <div className="grid grid-cols-2 gap-4">
-                    <div>
-                        <div className="text-sm text-gray-500 dark:text-gray-400">班级排名/等第</div>
-                        <div
-                            className="font-medium">{(paperRankInfo) ? (props.advancedMode) ? paperRankInfo.rank.class + " (打败了全班" + paperRankInfo.defeatRatio.class + "%的人)" : paperRankInfo.rankPart.class : "..."}</div>
-                    </div>
-                    <div>
-                        <div className="text-sm text-gray-500 dark:text-gray-400">年级排名/等第</div>
-                        <div
-                            className="font-medium">{(paperRankInfo) ? (props.advancedMode) ? paperRankInfo.rank.grade + " (打败了全年级" + paperRankInfo.defeatRatio.grade + "%的人)" : paperRankInfo.rankPart.grade : "..."}</div>
-                    </div>
-                </div>
-                <div className="grid grid-cols-2 gap-4">
-                    <div>
-                        <div className="text-sm text-gray-500 dark:text-gray-400">班级最高分</div>
-                        <div
-                            className="font-medium">{(paperRankInfo) ? (props.advancedMode) ? paperRankInfo.highest.class : "根据要求，该数据不允许展示" : "..."}</div>
-                    </div>
-                    <div>
-                        <div className="text-sm text-gray-500 dark:text-gray-400">年级最高分</div>
-                        <div
-                            className="font-medium">{(paperRankInfo) ? (props.advancedMode) ? paperRankInfo.highest.grade : "根据要求，该数据不允许展示" : "..."}</div>
-                    </div>
-                </div>
-                <div className="grid grid-cols-2 gap-4">
-                    <div>
-                        <div className="text-sm text-gray-500 dark:text-gray-400">班级平均分</div>
-                        <div
-                            className="font-medium">{(paperRankInfo) ? (props.advancedMode) ? paperRankInfo.avg.class : "根据要求，该数据不允许展示" : "..."}</div>
-                    </div>
-                    <div>
-                        <div className="text-sm text-gray-500 dark:text-gray-400">年级平均分</div>
-                        <div
-                            className="font-medium">{(paperRankInfo) ? (props.advancedMode) ? paperRankInfo.avg.grade : "根据要求，该数据不允许展示" : "..."}</div>
-                    </div>
-                </div>
-                <div data-html2canvas-ignore="true" className="grid grid-flow-col gap-4">
-                    {answerPictureUrls.map((url, index) => {
-                        // eslint-disable-next-line @next/next/no-img-element
-                        return <a key={index} href={url} target={"_blank"}><img className="rounded-lg object-cover"
-                                                                                src={url}
-                                                                                alt={props.paper.name + "_" + index}
-                                                                                width={300} style={{
-                            aspectRatio: "300/200",
-                            objectFit: "cover"
-                        }}
-                                                                                height={200}/></a>
-                    })}
-                </div>
-            </CardContent>
-        </Card>
-    )
-}
+import {SubjectHidingComponent, SubjectShowingComponent} from "@/app/exam/[id]/subject";
 
 export default function ExamPage({params}: { params: { id: string } }) {
     ChartJS.register(
@@ -308,7 +161,8 @@ export default function ExamPage({params}: { params: { id: string } }) {
                             <div onClick={() => {
                                 toast.promise(createScreenshot(), {
                                     loading: "正在截图",
-                                    success: "成功创建并下载截图！（答题卡图片空白是正常的）",
+                                    // success: "成功创建并下载截图！（答题卡图片空白是正常的）",
+                                    success: <span>成功创建并下载截图！<br/>(答题卡图片空白是正常的)</span>,
                                     error: (err) => "创建截图失败，原因：" + err
                                 }, {
                                     error: {
@@ -407,12 +261,12 @@ export default function ExamPage({params}: { params: { id: string } }) {
                                 let component
                                 (!displayedPapersMode[item.paperId]) ?
                                     component =
-                                        <PaperHiddingComponent paper={item} changeDisplayMode={changeDisplayedMode}
-                                                               key={item.paperId}/>
+                                        <SubjectHidingComponent paper={item} changeDisplayMode={changeDisplayedMode}
+                                                                key={item.paperId}/>
                                     : component =
-                                        <PaperShowingComponent paper={item} changeDisplayMode={changeDisplayedMode}
-                                                               advancedMode={advancedMode} router={router}
-                                                               examId={String(examDetail?.examId)} key={item.paperId}/>
+                                        <SubjectShowingComponent paper={item} changeDisplayMode={changeDisplayedMode}
+                                                                 advancedMode={advancedMode} router={router}
+                                                                 examId={String(examDetail?.examId)} key={item.paperId}/>
                                 return component
                             })}
                         </div>
